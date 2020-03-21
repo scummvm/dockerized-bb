633a27a030bb0f0d5c4a408f943bbbb7cbd99a8d
Le Philousophe <lephilousophe@users.noreply.github.com>
2020-03-21 17:07:43 +0000

    COMMON/SWITCH: Update fluidsynth-lite and add Switch support to it

--- toolchains/common/packages/fluidsynth-lite/build.sh
+++ toolchains/common/packages/fluidsynth-lite/build.sh
@@ -7,7 +7,7 @@ HELPERS_DIR=$PACKAGE_DIR/../..
 do_make_bdir
 
 do_http_fetch fluidsynth-lite \
-	'https://github.com/Doom64/fluidsynth-lite/archive/38353444676a1788ef78eb7f835fba4fa061f3f2.tar.gz' 'tar xzf'
+	'https://github.com/Doom64/fluidsynth-lite/archive/c539a8d9270ba5a3f7d6e460606483fc2ab1eb61.tar.gz' 'tar xzf'
 
 # -DCMAKE_SYSTEM_NAME=Windows for Windows
 
