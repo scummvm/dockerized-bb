4cdbe027f769550c6b3097f5e294433f6b632eec
Le Philousophe <lephilousophe@users.noreply.github.com>
2020-06-01 10:08:54 +0000

    PSP: Update toolchain

--- toolchains/psp/packages/toolchain/build.sh
+++ toolchains/psp/packages/toolchain/build.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-TOOLCHAIN_VERSION=a435ee0df19d8e07fa4c0a01f1f47fb7aeca834d
+TOOLCHAIN_VERSION=a6b7a4d315c71816ff0d8abc6054b03af75e7bd9
 
 PACKAGE_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 HELPERS_DIR=$PACKAGE_DIR/../..
@@ -16,8 +16,8 @@ export PATH=$PATH:$PSPDEV/bin
 export ACLOCAL_PATH=$PREFIX/share/aclocal:$ACLOCAL_PATH
 
 # Use -e to stop on error
-# Don't write environment variables to profile.d
-bash -e ./toolchain.sh $(seq 1 11)
+# Don't write environment variables to profile.d (like in toolchain-local.sh)
+bash -e ./toolchain.sh $(seq 1 12)
 
 do_clean_bdir
 
