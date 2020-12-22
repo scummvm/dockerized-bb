c5766b8016b668b2bfaeb7b54711e411383cef29
Le Philousophe <lephilousophe@users.noreply.github.com>
2020-12-22 20:49:59 +0000

    MASTER: Add file based and Github authentication support

--- master/ui.py
+++ master/ui.py
@@ -7,9 +7,6 @@ from buildbot.plugins import reporters
 
 import config
 
-htfile = os.path.join(config.buildbot_base_dir, config.htfile)
-web_authz = None
-
 www = {
     'plugins': dict(waterfall_view={}, console_view={}, grid_view={}),
 # TODO:
@@ -27,13 +24,45 @@ if hasattr(config, 'github_webhook_secret'):
         'strict': bool(config.github_webhook_secret),
     }
 
-if os.path.exists(htfile):
-    www['authz'] = util.Authz(auth=util.HTPasswdAuth(htfile),
-            forceBuild='auth', # only authenticated users
-            forceAllBuilds='auth', # only authenticated users
-            stopBuild='auth', # only authenticated users
-            stopAllBuilds='auth', # only authenticated users
-            cancelPendingBuild='auth', # only authenticated users
+if hasattr(config, 'ht_auth_file') and config.ht_auth_file:
+    ht_auth_file = os.path.join(config.buildbot_base_dir, config.ht_auth_file)
+    www['auth'] = util.HTPasswdAuth(ht_auth_file)
+    # When using htpasswd file, we don't have any group information nor email information
+    www['authz'] = util.Authz(
+        stringsMatcher=util.fnmatchStrMatcher,  # simple matcher with '*' glob character
+        allowRules=[
+            # admins can do anything,
+            # defaultDeny=False: if user does not have the admin role, we continue parsing rules
+            util.AnyEndpointMatcher(role="admin", defaultDeny=False),
+            # if future Buildbot implement new control, we are safe with this last rule
+            util.AnyControlEndpointMatcher(role="admin")
+        ],
+        roleMatchers=[
+            util.RolesFromUsername(roles=['admin'], usernames=config.ht_auth_admins),
+        ]
+    )
+elif hasattr(config, 'github_auth_clientid') and config.github_auth_clientid:
+    www['auth'] = util.GitHubAuth(
+            config.github_auth_clientid, config.github_auth_clientsecret,
+            apiVersion=4, getTeamsMembership=True)
+    # When using Github authentication, we can use group membership information
+    www['authz'] = util.Authz(
+        stringsMatcher=util.fnmatchStrMatcher,  # simple matcher with '*' glob character
+        # stringsMatcher = util.reStrMatcher,   # if you prefer regular expressions
+        allowRules=[
+            # admins can do anything,
+            # defaultDeny=False: if user does not have the admin role, we continue parsing rules
+            util.AnyEndpointMatcher(role=config.github_admin_group, defaultDeny=False),
+            # Let owner stop its build
+            util.StopBuildEndpointMatcher(role="owner"),
+            # if future Buildbot implement new control, we are safe with this last rule
+            util.AnyControlEndpointMatcher(role=config.github_admin_group)
+        ],
+        roleMatchers=[
+            util.RolesFromGroups(groupPrefix="{0}/".format(config.github_organisation)),
+            # role owner is granted when property owner matches the email of the user
+            util.RolesFromOwner(role="owner")
+        ]
     )
 
 try:
