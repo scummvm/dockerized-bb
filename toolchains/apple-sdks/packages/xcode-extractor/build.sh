bab67b8cd0b8612b9f8bcba90f9a1bb86cd9c915
Le Philousophe <lephilousophe@users.noreply.github.com>
2021-01-24 20:00:44 +0000

    APPLE-SDKS: Make packages extraction more efficient
    It now takes way less disk space

--- toolchains/apple-sdks/packages/xcode-extractor/build.sh
+++ toolchains/apple-sdks/packages/xcode-extractor/build.sh
@@ -15,13 +15,13 @@ do_git_fetch osxcross "https://github.com/tpoechtrager/osxcross.git" "${OSXCROSS
 # There is a bug in Debian Stretch docker package which prevents having too big files
 # Split the file and join back it there
 if [ "$(ls -1 "${PACKAGES_LOCATION}${PACKAGE}"* | wc -l)" -gt 1 ]; then
-	XCODE_FILE=$(mktemp -p $(pwd))
-	cat "${PACKAGES_LOCATION}${PACKAGE}"* > $XCODE_FILE
+	XCODE_FILE=$(mktemp -up $(pwd))
+	mkfifo -m 600 "$XCODE_FILE"
+	cat "${PACKAGES_LOCATION}${PACKAGE}"* | USESYSTEMCOMPILER=yes CC=gcc ./tools/gen_sdk_package_pbzx.sh /dev/stdin
 else
 	XCODE_FILE=$(echo "${PACKAGES_LOCATION}${PACKAGE}"*)
+	USESYSTEMCOMPILER=yes CC=gcc ./tools/gen_sdk_package_pbzx.sh "$XCODE_FILE"
 fi
-
-USESYSTEMCOMPILER=yes CC=gcc ./tools/gen_sdk_package_pbzx.sh "$XCODE_FILE"
 
 mkdir -p "${SDK_DIR}"
 mv ./*.sdk.* "${SDK_DIR}"/
