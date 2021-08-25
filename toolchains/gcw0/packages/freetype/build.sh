3484dde24384083331ba2b3b81955e7f0a451a50
Le Philousophe <lephilousophe@users.noreply.github.com>
2021-08-25 11:13:00 +0000

    GCW0: Fix Freetype download URL

--- toolchains/gcw0/packages/freetype/build.sh
+++ toolchains/gcw0/packages/freetype/build.sh
@@ -11,8 +11,8 @@ do_make_bdir
 
 # GPG key of Werner Lemberg <wl@gnu.org> 
 gpg --keyserver hkps://keyserver.ubuntu.com --recv-keys 0xC1A60EACE707FDA5
-do_http_fetch freetype "http://download.savannah.gnu.org/releases/freetype/freetype-${FREETYPE_VERSION}.tar.bz2" \
-       'tar xjf' "gpgurl:http://download.savannah.gnu.org/releases/freetype/freetype-${FREETYPE_VERSION}.tar.bz2.sig"
+do_http_fetch freetype "http://download.savannah.gnu.org/releases/freetype/freetype-old/freetype-${FREETYPE_VERSION}.tar.bz2" \
+       'tar xjf' "gpgurl:http://download.savannah.gnu.org/releases/freetype/freetype-old/freetype-${FREETYPE_VERSION}.tar.bz2.sig"
 rm -Rf $HOME/.gnupg
 
 do_configure_shared
