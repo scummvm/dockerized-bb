03efab3454f3bc724f3de0ebf28974472a13f19a
Le Philousophe <lephilousophe@users.noreply.github.com>
2024-09-07 11:02:56 +0000

    ANDROID: Use newer NDK version 23

--- toolchains/android/packages/ndk/build.sh
+++ toolchains/android/packages/ndk/build.sh
@@ -17,14 +17,8 @@ do_sdk_install "ndk;${ANDROID_NDK_VERSION}"
 
 cd "ndk/${ANDROID_NDK_VERSION}"
 
-# Cleanup unused parts of the NDK (which will get removed in future versions)
-rm -rf platforms sources/cxx-stl sysroot
-for d in toolchains/*; do
-	if [ "$d" = "toolchains/llvm" ]; then
-		continue
-	fi
-	rm -rf "$d"
-done
+# Cleanup unused parts of the NDK
+rm -rf prebuilt sources/cxx-stl sources/third_party simpleperf shader-tools toolchains/renderscript
 
 mkdir -p "${ANDROID_NDK_ROOT}/"
 # mv is faster than cp
